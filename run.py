3266b423099ddafd1612386b991e65aa3203dd3b
Tom Buckley <buckley.tom@gmail.com>
2016-04-14 16:27:54 -0700

    turn off slack/maps unless on ec2

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ warnings.filterwarnings("ignore")
 
 args = sys.argv[1:]
 
-SLACK = MAPS = True
+SLACK = MAPS = True if os.environ['SLACKCHECK']=='YESPLEASE' else False
 LOGS = True
 INTERACT = False
 SCENARIO = None
