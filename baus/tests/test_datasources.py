069ae1ed327fcb2d3101503ccb88d58d6d8a4061
MTC-yw <ywang@bayareametro.gov>
2020-07-03 11:53:35 -0700

    Revert "Merge branch 'run-staging' into db_geographies_summary"
    This reverts commit 975b04884b51ed246e277dc5829d7d276dd6426f, reversing
    changes made to 4d1c46743d158ee8d3d81de7c93677198fecc06f.

--- baus/tests/test_datasources.py
+++ baus/tests/test_datasources.py
@@ -40,9 +40,7 @@ def test_inclusionary_housing_settings():
         }]
     }
     out = datasources.inclusionary_housing_settings({
-        "inclusionary_housing_settings": inclusionary_housing_settings,
-        "inclusionary_fr2_enable": "foo",
-        "inclusionary_d_b_enable": "foo"
+        "inclusionary_housing_settings": inclusionary_housing_settings
     }, "foo")
 
     assert out["Berkeley"] == .2
