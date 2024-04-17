8c3fe14facf4d14115777e251d7810fa6277b663
Aksel Olsen <akselkolsen@gmail.com>
2024-04-17 08:59:22 -0700

    Added run log extractor script to get key run related data

---
+++ scripts/create_run_log_from_yamls.py
@@ -0,0 +1,162 @@
+import pandas as pd
+from pathlib import Path
+import pathlib
+import yaml
+import datetime
+import os
+
+
+# Script recursively searches for yaml run setup files and extracts the key BAUS run setup information
+# for post-hoc meta-log of runs. It prepares a csv including runtime data from the yamls themselves,
+# along with timestamps from the file creation as well as from the log file (for start and finish), 
+# to flag whether a given run was run to completion.
+
+
+def get_log_file_from_yaml_path(p: Path) -> Path:
+    """
+    Constructs the path to the log file from a run setup YAML file path.
+
+    This function assumes the log file shares the same stem as the YAML file, 
+    but with a `.log` extension, and potentially without prefixes/suffixes 
+    containing "run_setup".
+
+    Args:
+        p (Path): The path to the run setup YAML file.
+
+    Returns:
+        Path: The path to the corresponding log file.
+    """
+
+    # use the yaml file to create the path to the log file
+    p_log_file =  f'{p.stem}.log'.replace('_run_setup', '').replace('run_setup_', '')
+    p_log_path = p.parent /  p_log_file
+    return p_log_path
+
+
+def get_timestamp_from_run_log(p_log_path: Path) -> dict:
+    """
+    Parses a log file to get timestamps of start and finish of a run.
+
+    Args:
+        p_log_path (Path): The path object to the log file.
+
+    Returns:
+        dict: A dictionary containing 'start' and 'finish' timestamps in ISO format 
+              or None if not found.
+    """
+
+    output = {'start': None, 'finish': None}
+
+    # open the file object
+    fobj = open(p_log_path, "r")
+
+    # read the lines in the log
+    log_file = fobj.readlines()
+
+    # turn to pd.Series for easy searching for start and finish text markers
+    log_file_s = pd.Series(log_file)
+    log_file_start = log_file_s[log_file_s.str.contains('^Started')]
+    log_file_finish = log_file_s[log_file_s.str.contains('^Finish')]
+
+    # If the started string is found in the log, get it:
+    if len(log_file_start) > 0:
+        start_time = pd.to_datetime(log_file_start.str.replace(
+            'Started ', '').str.replace('\n', '')).iloc[0].isoformat()
+        output['start'] = start_time
+    
+    # If the finished string is found in the log, get it:
+    if len(log_file_finish) > 0:
+        finish_time = pd.to_datetime(log_file_finish.str.replace(
+            'Finished ', '').str.replace('\n', '')).iloc[0].isoformat()
+        output['finish'] = finish_time
+
+    # log_date_format = "%a %b %d %H:%M:%S %Y"
+    # parsed_date = datetime.datetime.strptime(log_date_string, log_date_format)
+    # return parsed_date.isoformat()
+    return output
+
+def build_run_log(root_dir: Path, m_out_path: Path, box_out_path: Path) -> pd.DataFrame:
+    """
+    Builds a DataFrame of run logs from a directory.
+
+    Args:
+        root_dir (Path): The path to the root directory containing run logs.
+        m_out_path (Path: The path to the output CSV file for ML.
+        box_out_path (Path): The path to the output CSV file for Box.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing information about each run log.
+    """
+
+    dicts = []
+
+    for p in root_dir.rglob("*run_setup*.yaml"):
+        print(p)
+
+        # we skip the occasional run_setup.yaml without suffixes as not being actual run logs
+        # as well as any repo yaml
+        if p.name == 'run_setup.yaml' or 'bayarea_urbansim' in str(p.parent):
+            print(f'Skipping {p}')
+            continue
+
+        # get file stats
+        this_stat = p.stat()
+
+        # keep timestamp
+        file_birth_ts = this_stat.st_birthtime
+
+        file_birth_dt = datetime.datetime.fromtimestamp(file_birth_ts)
+        ts_iso = file_birth_dt.isoformat()
+
+        with open(p, 'r') as f:
+            this_dict = yaml.safe_load(f)
+
+        # get log file from yaml path for start and end time
+        try:
+            p_log_path = get_log_file_from_yaml_path(p)
+
+            # then get the time stamp from the log file
+            ts_dict = get_timestamp_from_run_log(p_log_path)
+
+            this_dict['time_stamp_start'] = ts_dict['start']
+            this_dict['time_stamp_end'] = ts_dict['finish']
+
+            print(ts_dict['start'])
+
+        except (FileNotFoundError, ValueError) as error:
+            print(error)
+            print(f'{p_log_path} not found')
+            ts_iso = None
+
+        this_dict['yaml_path'] = p
+        this_dict['time_stamp_start_pth'] = ts_iso
+
+        dicts.append(this_dict)
+    df = pd.DataFrame.from_records(dicts)
+
+    df['is_complete'] = df.time_stamp_start_pth.notna()
+
+    # Put meta columns up front
+
+if __name__ == "__main__":
+
+    # PATHS 
+
+    # set the path for M: drive
+    # from OSX, M:/ may be mounted to /Volumes/Data/Models
+    M_DRIVE = Path("/Volumes/Data/Models") if os.getlogin()=='aolsen' else Path("M:/")
+    HOME_DIR = pathlib.Path.home()
+    BOX_DIR = HOME_DIR / 'Box' if not os.getlogin()=='lzorn' else Path("E:/Box")
+
+    #output paths
+    
+    m_out_path = M_DRIVE / "urban_modeling" / \
+        "baus" / "PBA50Plus" / 'run_setup_tracker_autogen.csv'
+    
+    box_out_path = BOX_DIR / "Modeling and Surveys" / "Urban Modeling" / \
+        "Bay Area UrbanSim" / "PBA50plus Meta" / 'run_setup_tracker_autogen.csv'
+
+    root_dir = M_DRIVE / "urban_modeling" / "baus" / "PBA50Plus" 
+
+    # call the function
+    build_run_log(root_dir, m_out_path, box_out_path)
