d02eccb30356c0cf6d4eb08d464218b103e3c5ad
Aksel Olsen <akselkolsen@gmail.com>
2024-04-30 12:24:56 -0700

    Restructured transit_service_area_share() to not hard code 2050 but add data for the base year

--- scripts/metrics/metrics_connected.py
+++ scripts/metrics/metrics_connected.py
@@ -21,71 +21,10 @@ def transit_service_area_share(
 
                  
     # TODO: we need a 2015 / 2020 / 2023 as well
-    year = 2050
+    #year = 2050
     logging.info(f"Calculating connected for {modelrun_alias} / {modelrun_id}")
     logging.debug(f"Modelrun data years: {modelrun_data.keys()}")
     logging.debug(f"Modelrun data 2050 datasets: {modelrun_data[2050].keys()}")
-
-    parcel_output = modelrun_data[2050]["parcel"]
-    # report shape of parcel_output df
-    len_parcels = len(parcel_output)
-
-    logging.debug('Cols of parcels 2050 in connected func: {}'.format(parcel_output.columns))
-    logging.debug(f"Parcel output has {len_parcels:,} rows")
-
-    
-    
-    #SUMMARY_YEARS = sorted(modelrun_data.keys())
-    #print(SUMMARY_YEARS)
-
-
-    # The transit_scenario_mapping relates the run type to which transit stop buffer universe is appropriate
-    # e.g. final blueprint simulation runs should be matched with similar scenario transit stops / headways.
-    
-    # Left hand side is modelrunid_alias - we want to relate that to the specific transit stop map to use
-    # where stops and headways will differ for np and dbp. The parcel crosswalk has different classifications
-    # for np and dbp.
-    transit_scenario_mapping = {
-        "NoProject": "np",
-        "No Project": "np",
-        "Plus": "fbp",
-        "Final Blueprint": "fbp",
-        "Draft Blueprint": "fbp",
-        "DBP": "fbp", # we map to FBP in the transit service class file
-        "Alt1": "fbp",
-        "Alt2": "fbp",
-        "EIR Alt 1":'fbp',
-        "EIR Alt 2":'fbp',
-        "Current": "cur",
-    }
-
-    # get the transit scenario to focus on (e.g., 'fbp' for final bluerprint)
-    # we really need to have a distinct fbp/eir, etc designation from run id in the log
-    transit_scenario = transit_scenario_mapping.get(modelrun_alias,'fbp')
-
-    # set transit_scenario to cur (existing stops buffers) 
-    transit_scenario = "cur" if int(year) in [2015, 2020] else transit_scenario
-
-    # Identify the passed scenario-specific columns (fbp no project, current)
-    # this returns different classifications for each - like the 5-way or 6-way service level (cat5, cat6)
-    # several may be returned depending on how many are in the crosswalk
-    # we summarize run data for each classification variable
-
-    transit_svcs_cols = parcel_output.filter(
-        regex=transit_scenario
-    ).columns.tolist()
-
-    logging.debug(
-        f'Transit scenario specific for {transit_scenario=} classifier columns: {"; ".join(transit_svcs_cols)}'
-    )
-
-    # Define columns containing values of interest - more could be added as long as it is present and numeric
-    val_cols = ["totemp", "RETEMPN", "MWTEMPN","OTHEMPN","HEREMPN","FPSEMPN", "tothh"]
-    agg_mapping = {col: 'sum' for col in val_cols}
-
-    # Fill missing values and convert specific columns to integers for consistency
-    for col in val_cols:
-        parcel_output[col] = parcel_output[col].fillna(0).round(0).astype(int)
 
     # convenience function for easy groupby percentages
     def pct(x):
@@ -110,50 +49,125 @@ def transit_service_area_share(
         grp_summary_shares = (
             grp_summary
             .groupby(level=group_vars[0],
-                     group_keys=False)
+                    group_keys=False)
             .apply(pct)
             .round(3)
         )
 
         return grp_summary_shares
 
+    # The transit_scenario_mapping relates the run type to which transit stop buffer universe is appropriate
+    # e.g. final blueprint simulation runs should be matched with similar scenario transit stops / headways.
+    
+    # Left hand side is modelrunid_alias - we want to relate that to the specific transit stop map to use
+    # where stops and headways will differ for np and dbp. The parcel crosswalk has different classifications
+    # for np and dbp.
+
+    this_modelrun_alias = metrics_utils.classify_runid_alias(modelrun_alias)
+
+    transit_scenario_mapping = {
+        "NoProject": "np",
+        "No Project": "np", # allowing for variations in input here
+        "NP":'np',
+        "Plus": "fbp",
+        "Final Blueprint": "fbp",
+        "Draft Blueprint": "fbp",
+        "DBP": "fbp", # we map to FBP in the transit service class file
+        "Alt1": "fbp",
+        "Alt2": "fbp",
+        "EIR Alt 1":'fbp',
+        "EIR Alt 2":'fbp',
+        "Current": "cur", # refers to existing conditions transit stops
+    }
+
+    # get the transit scenario to focus on (e.g., 'fbp' for final bluerprint)
+    # we really need to have a distinct fbp/eir, etc designation from run id in the log
+    transit_scenario = transit_scenario_mapping.get(this_modelrun_alias,'fbp')
+
+    # Define columns containing values of interest - more could be added as long as it is present and numeric
+    val_cols = ["totemp", "RETEMPN", "MWTEMPN","OTHEMPN","HEREMPN","FPSEMPN", "tothh", "hhq1"]
+    # use for groupby later to say what do do with each col - we just sum them
+    agg_mapping = {col: 'sum' for col in val_cols}
+
+
+    SUMMARY_YEARS = sorted(modelrun_data.keys())
+
+    # for storing yearly data
     container = {}
 
-    # Now for the summaries - a variable (e.g. tothh) for a geographic area (e.g. region, hra, etc.)
-    # is distributed by transit service area (e.g. cat5, cat6, etc.): what share of households
-    # live in major transit areas? what share of jobs live in major transit areas?
 
-    if rtp=="RTP2021":
-        # RTP2021 was tract10-based
-        area_vars = {'region': "Region", 'tract10_epc': 'CoCs',
-                    'tract10_hra': 'HRAs', 'area_type': 'area_type'}
-        parcel_output['tract10_hra'] = parcel_output['tract10_hra'].replace({0:'Not HRA',1:'HRAs'})
-        parcel_output['tract10_epc'] = parcel_output['tract10_epc'].replace({0:'Not EPC',1:'EPCs'})
-    if rtp=="RTP2025":
-        area_vars = {'region': "Region", 'tract20_epc': 'CoCs',
-                    'tract20_hra': 'HRAs', 'area_type': 'area_type'}
-        parcel_output['tract20_hra'] = parcel_output['tract20_hra'].replace({0:'Not HRA',1:'HRAs'})
-        parcel_output['tract20_epc'] = parcel_output['tract20_epc'].replace({0:'Not EPC',1:'EPCs'})
+    for year in SUMMARY_YEARS:
 
-    
-    # Add constant useful for groupby summaries across the whole region
-    parcel_output['region'] = 'Region'
-    
-    # loop through combinations of area types and transit service area classifications
-    for combo in product(*[area_vars, transit_svcs_cols]):
+        # set transit_scenario to cur (existing stops buffers) 
+        transit_scenario = "cur" if int(year) in [2015, 2020, 2023] else transit_scenario
 
-        # run summary on this combination
-        logging.debug(f"Running parcel summaries for {'-'.join(combo)}")
-        this_summary_servicelevel = groupby_summaries(
-            parcel_output, list(combo)
+        parcel_output = modelrun_data[year]["parcel"]
+        # report shape of parcel_output df
+        len_parcels = len(parcel_output)
+
+        logging.debug('Cols of parcels {} in connected func: {}'.format(year,parcel_output.columns))
+        logging.debug(f"Parcel output has {len_parcels:,} rows")
+
+        
+
+        # Identify the passed scenario-specific columns (fbp no project, current)
+        # this returns different classifications for each - like the 5-way or 6-way service level (cat5, cat6)
+        # several may be returned depending on how many are in the crosswalk
+        # we summarize run data for each classification variable
+
+        transit_svcs_cols = parcel_output.filter(
+            regex=transit_scenario
+        ).columns.tolist()
+
+        logging.debug(
+            f'Transit scenario specific for {transit_scenario=} classifier columns: {"; ".join(transit_svcs_cols)}'
         )
 
-        # new key, including year
-        this_key = (year,) + combo
-        container[this_key] = this_summary_servicelevel
-        logging.debug(f"Summary for {this_key} is {this_summary_servicelevel}")
 
-    # concat the dict of dataframes into a single dataframe
+        # Fill missing values and convert specific columns to integers for consistency
+        for col in val_cols:
+            parcel_output[col] = parcel_output[col].fillna(0).round(0).astype(int)
+
+        # convenience function for easy groupby percentages
+
+        # Now for the summaries - a variable (e.g. tothh) for a geographic area (e.g. region, hra, etc.)
+        # is distributed by transit service area (e.g. cat5, cat6, etc.): what share of households
+        # live in major transit areas? what share of jobs "live" in major transit areas?
+
+        if rtp=="RTP2021":
+            # RTP2021 was tract10-based
+            area_vars = {'region': "Region", 'tract10_epc': 'CoCs',
+                        'tract10_hra': 'HRAs', 'area_type': 'area_type'}
+            parcel_output['tract10_hra'] = parcel_output['tract10_hra'].replace({0:'Not HRA',1:'HRAs'})
+            parcel_output['tract10_epc'] = parcel_output['tract10_epc'].replace({0:'Not EPC',1:'EPCs'})
+        if rtp=="RTP2025":
+            area_vars = {'region': "Region", 'tract20_epc': 'CoCs',
+                        'tract20_hra': 'HRAs', 'area_type': 'area_type'}
+            parcel_output['tract20_hra'] = parcel_output['tract20_hra'].replace({0:'Not HRA',1:'HRAs'})
+            parcel_output['tract20_epc'] = parcel_output['tract20_epc'].replace({0:'Not EPC',1:'EPCs'})
+
+        
+        # Add constant useful for constistent handling of groupby summaries 
+        # across the whole region
+        parcel_output['region'] = 'Region'
+        
+        # main event - loop through combinations of area types and transit service area classifications
+        for combo in product(*[area_vars, transit_svcs_cols]):
+
+            # run summary on this combination
+            logging.debug(f"Running parcel summaries for {'-'.join(combo)}")
+            this_summary_servicelevel = groupby_summaries(
+                parcel_output, list(combo)
+            )
+
+            # new key, including year - making a new tuple
+            this_key = (year,) + combo
+            
+            # store in the dict
+            container[this_key] = this_summary_servicelevel
+            logging.debug(f"Summary for {this_key} is {this_summary_servicelevel}")
+
+    # collect the dict of dataframes from each year into a single dataframe
     container_df = pd.concat(
         container,
         names=[
@@ -168,6 +182,7 @@ def transit_service_area_share(
     
     # turn to long format series w multi-index
     container_df = container_df.stack()
+
     logging.debug(f"head for (wide) {container_df.head()}")
 
     container_df = (
@@ -237,7 +252,7 @@ def format_for_tableau(plan_metrics_df: pd.DataFrame,
     logging.debug(f"Query string: {qrystring}")
     logging.debug(f"Number of metrics: {len(plan_metrics_df)}")
 
-    tableau_column_map = { 'modelrun_id':'modelrun_id',
+    tableau_column_map = { 'year':'year','modelrun_id':'modelrun_id',
         "modelrun_alias": "modelrun_alias",
         "area_grouping": "area_grouping",
         "transit_grouping": "transit_grouping",
@@ -295,7 +310,7 @@ def format_for_tableau(plan_metrics_df: pd.DataFrame,
     )
 
     # prepare output schema
-    id_cols = ["modelrun_id","modelrun_alias", "type_code", "type_group", "type_subgroup"]
+    id_cols = ["year","modelrun_id","modelrun_alias", "type_code", "type_group", "type_subgroup"]
     id_extra_cols = [
         "area_grouping",
         "transit_grouping",
