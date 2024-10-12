65bec68fe8b1a495da6d05b8212996f1f8e38fb2
Aksel Olsen <akselkolsen@gmail.com>
2024-10-12 07:10:54 -0700

    adding extra alt_feasibility steps for now

--- baus.py
+++ baus.py
@@ -285,15 +285,15 @@ def run_models(mode, run_setup, years_to_run):
         #        "subsidized_residential_feasibility",
         #        "subsidized_residential_developer_jobs_housing",
 
-                #"alt_feasibility",
+                "alt_feasibility",
                 "residential_developer",
                 "developer_reprocess",
 
-                #"alt_feasibility",
+                "alt_feasibility",
                 
                 "retail_developer",
 
-                #"alt_feasibility",
+                "alt_feasibility",
                 
                 "office_developer",
                 "subsidized_office_developer_vmt",
