3c5879fe49712ec1a202358ba29de84726e4cb3b
Lisa Zorn <lzorn@bayareametro.gov>
2024-10-23 12:30:37 -0700

    Convert print to logger.debug and log acct_settings, limit_settings, coffer

--- baus/debug.py
+++ baus/debug.py
@@ -10,15 +10,12 @@ import pandas as pd
 # and shouldn't be used in production runs
 
 @orca.step()
-def debug(base_year, year, nodes, parcels, buildings, residential_units, households, jobs, zones):
-    print("year={}".format(year))
+def debug(store, base_year, year, nodes, parcels, buildings, residential_units, households, jobs, zones, 
+    acct_settings, limits_settings, coffer, logger):
+
+    logger.debug("year={}".format(year))
     # parcels and buildings are instances of a DataFrameWrapper
     # https://udst.github.io/orca/core.html#orca.orca.DataFrameWrapper
-
-    nodes_columns = sorted(list(nodes.columns))
-    print("nodes.columns: {}".format(nodes_columns))
-    for node_col in nodes_columns:
-        print("node_col {} type={}".format(node_col, nodes.column_type(node_col)))
 
     output_store = None
     # create the 2020 input file for 2020 start (assuming this is not a 2020 start)
@@ -30,10 +27,53 @@ def debug(base_year, year, nodes, parcels, buildings, residential_units, househo
             / now.strftime("%Y_%m_%d_bayarea_2020start.h5")
         )
         output_store = pd.HDFStore(h5_path, "w")
-        print("Creating {}".format(h5_path))
+        logger.debug("Creating {}".format(h5_path))
+
+    nodes_columns = sorted(list(nodes.columns))
+    logger.debug("nodes.columns: {}".format(nodes_columns))
+    for node_col in nodes_columns:
+        logger.debug("node_col {} type={}".format(node_col, nodes.column_type(node_col)))
+    if (year == 2020) and (base_year != 2020):
+        nodes_table_columns = [
+           'ave_hhsize',
+           'ave_income_1500',
+           'ave_income_500',
+           'ave_lot_size_per_unit',
+           'ave_sqft_per_unit',
+           'industrial',
+           'industrial_1500',
+           'jobs_1500',
+           'jobs_500',
+           'office',
+           'office_1500',
+           'poor',
+           'population',
+           'renters',
+           'residential',
+           'residential_units_1500',
+           'residential_units_500',
+           'retail',
+           'retail_1500',
+           'retail_sqft_3000',
+           'sfdu',
+           'sum_income_3000'
+        ]
+        for node_col in nodes_table_columns:
+            logger.debug("node_col {} type={}".format(node_col, nodes.column_type(node_col)))
+        if output_store != None:
+            node_store_df = nodes.to_frame(nodes_table_columns)
+            logger.debug("node_store_df.dtypes:\n{}".format(node_store_df.dtypes))
+            output_store["nodes"] = node_store_df
+    # TODO: this belongs elsewhere
+    if (year==2020) and (base_year == 2020):
+        nodes_df = store['nodes']
+        logger.debug('retrieving nodes_df from store:\n')
+        logger.debug(nodes_df.head())
+        logger.debug(nodes_df.describe().apply(lambda s: s.apply('{0:.2f}'.format)))
+        orca.add_table("nodes", nodes_df)
 
     parcels_columns = sorted(list(parcels.columns))
-    print("parcels.columns: {}".format(parcels_columns))
+    logger.debug("parcels.columns: {}".format(parcels_columns))
     # these are the columns in the original store
     parcel_store_columns = [
         'development_type_id',
@@ -52,14 +92,14 @@ def debug(base_year, year, nodes, parcels, buildings, residential_units, househo
         'shape_area'
     ]
     for parcel_col in parcel_store_columns:
-        print("parcel_col {} type={}".format(parcel_col, parcels.column_type(parcel_col)))
+        logger.debug("parcel_col {} type={}".format(parcel_col, parcels.column_type(parcel_col)))
     if output_store != None:
         parcel_store_df = parcels.to_frame(parcel_store_columns)
-        print("parcel_store_df.dtypes:\n{}".format(parcel_store_df.dtypes))
+        logger.debug("parcel_store_df.dtypes:\n{}".format(parcel_store_df.dtypes))
         output_store["parcels"] = parcel_store_df
 
     buildings_columns = sorted(list(buildings.columns))
-    print("buildings.columns: {}".format(buildings_columns))
+    logger.debug("buildings.columns: {}".format(buildings_columns))
     # these are the columns in the original store
     buildings_preproc_store_columns = [
         'parcel_id',
@@ -81,10 +121,10 @@ def debug(base_year, year, nodes, parcels, buildings, residential_units, househo
         'deed_restricted_units',
     ]
     for building_col in buildings_preproc_store_columns:
-        print("building_col {} type={}".format(building_col, buildings.column_type(building_col)))
+        logger.debug("building_col {} type={}".format(building_col, buildings.column_type(building_col)))
     if output_store != None:
         buildings_store_df = buildings.to_frame(buildings_preproc_store_columns)
-        print("buildings_store_df.dtypes:\n{}".format(buildings_store_df.dtypes))
+        logger.debug("buildings_store_df.dtypes:\n{}".format(buildings_store_df.dtypes))
         output_store["buildings_preproc"] = buildings_store_df
 
     residential_units_preproc_columns = [
@@ -97,15 +137,15 @@ def debug(base_year, year, nodes, parcels, buildings, residential_units, househo
         'tenure',
     ]
     for residential_unit_col in residential_units_preproc_columns:
-        print("residential_unit_col {} type={}".format(residential_unit_col, residential_units.column_type(residential_unit_col)))
+        logger.debug("residential_unit_col {} type={}".format(residential_unit_col, residential_units.column_type(residential_unit_col)))
     if output_store != None:
         residential_units_store_df = residential_units.to_frame(residential_units_preproc_columns)
-        print("residential_units_store_df.dtypes:\n{}".format(residential_units_store_df.dtypes))
+        logger.debug("residential_units_store_df.dtypes:\n{}".format(residential_units_store_df.dtypes))
         output_store["residential_units_preproc"] = residential_units_store_df
 
     households_columns = sorted(list(households.columns))
-    print("households.columns: {}".format(households_columns))
-    print("household_col {} type={}".format('node_id', households.column_type('node_id')))
+    logger.debug("households.columns: {}".format(households_columns))
+    logger.debug("household_col {} type={}".format('node_id', households.column_type('node_id')))
     # these are the columns in the original store
     households_preproc_store_columns = [
         'base_income_octile',
@@ -160,26 +200,34 @@ def debug(base_year, year, nodes, parcels, buildings, residential_units, househo
         'vehicl',
         'unit_num',
         'unit_id',
+        'move_in_year'
     ]
     for household_col in households_preproc_store_columns:
-        print("household_col {} type={}".format(household_col, households.column_type(household_col)))
+        logger.debug("household_col {} type={}".format(household_col, households.column_type(household_col)))
     if output_store != None:
         household_store_df = households.to_frame(households_preproc_store_columns)
-        print("household_store_df.dtypes:\n{}".format(household_store_df.dtypes))
+        logger.debug("household_store_df.dtypes:\n{}".format(household_store_df.dtypes))
         output_store["households_preproc"] = household_store_df
-    
+    if (year==2020):
+        households_table = orca.get_table("households")
+        households_temp_df = households_table.to_frame(['income','node_id'])
+        logger.debug("households['income','node_id']:\n")
+        logger.debug(households_temp_df)
+        logger.debug(households_temp_df.describe().apply(lambda s: s.apply('{0:.2f}'.format)))
+
     # these are the columns in the original store
     jobs_preproc_store_columns = [
         'sector_id',
         'empsix',
         'taz',
-        'building_id'
+        'building_id',
+        'move_in_year'
     ]
     for jobs_col in jobs_preproc_store_columns:
-        print("jobs_col {} type={}".format(jobs_col, jobs.column_type(jobs_col)))
+        logger.debug("jobs_col {} type={}".format(jobs_col, jobs.column_type(jobs_col)))
     if output_store != None:
         jobs_store_df = jobs.to_frame(jobs_preproc_store_columns)
-        print("jobs_store_df.dtypes:\n{}".format(jobs_store_df.dtypes))
+        logger.debug("jobs_store_df.dtypes:\n{}".format(jobs_store_df.dtypes))
         output_store["jobs_preproc"] = jobs_store_df
 
     # these are the columns in the original store
@@ -190,13 +238,25 @@ def debug(base_year, year, nodes, parcels, buildings, residential_units, househo
         'acres'
     ]
     for zones_col in zones_store_columns:
-        print("zones_col {} type={}".format(zones_col, zones.column_type(zones_col)))
+        logger.debug("zones_col {} type={}".format(zones_col, zones.column_type(zones_col)))
     if output_store != None:
         zones_store_df = zones.to_frame(zones_store_columns)
-        print("zones_store_df.dtypes:\n{}".format(zones_store_df.dtypes))
+        logger.debug("zones_store_df.dtypes:\n{}".format(zones_store_df.dtypes))
         output_store["zones"] = zones_store_df
 
-    
+    logger.debug("---- acct_settings ({}) ----".format(type(acct_settings)))
+    for acct_key in sorted(acct_settings.keys()):
+        logger.debug("{} -> {}".format(acct_key, acct_settings[acct_key]))
+
+    logger.debug("---- limits_settings ({}) ----".format(type(limits_settings)))
+    for limits_key in sorted(limits_settings.keys()):
+        logger.debug("{} -> {}".format(limits_key, limits_settings[limits_key]))
+
+    # coffer is a dictionary of Account objects
+    logger.debug("---- Coffer ({}) ----".format(type(coffer)))
+    for acct_name in sorted(coffer.keys()):
+        logger.debug("Coffer[{}]:\n{}".format(acct_name, coffer[acct_name]))
+
     # close it
     if output_store:
         output_store.close()
@@ -205,10 +265,10 @@ def debug(base_year, year, nodes, parcels, buildings, residential_units, househo
     try:
         # this may throw an exception if some variable calculations don't work
         parcels_df = parcels.to_frame()
-        print("parcels len {:,} dtypes:\n{}".format(len(parcels_df), parcels_df.dtypes))
+        logger.debug("parcels len {:,} dtypes:\n{}".format(len(parcels_df), parcels_df.dtypes))
     except Exception as e:
-        print("parcels.to_frame() exception raised")
-        print(type(e))
-        print(e)
-        print(traceback.format_exc())
+        logger.error("parcels.to_frame() exception raised")
+        logger.error(type(e))
+        logger.error(e)
+        logger.error(traceback.format_exc())
     return
