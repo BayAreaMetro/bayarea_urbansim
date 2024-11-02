b46f6147bb94724d261f077d5929f681e54e057b
Lisa Zorn <lzorn@bayareametro.gov>
2024-11-02 12:46:11 -0700

    Turn down logging for other files
    Prefix print statements for later conversion to log statements

--- logging_setup.py
+++ logging_setup.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import pandas as pd
 
 # Add this hack to capture stdout and sterr printouts to the logger
 class StreamToLogger:
@@ -10,13 +11,18 @@ class StreamToLogger:
 
     def write(self, buf):
         for line in buf.rstrip().splitlines():
-            self.logger.log(self.log_level, line)
+            # note as [p] so we can convert to logger lines
+            self.logger.log(self.log_level, "[p] " + line)
 
     def flush(self):
         pass  # Required for compatibility
 
 def setup_logging(log_file, log_level=logging.INFO):
     """Set up logging and redirect stdout/stderr to the logger."""
+
+    # make this wide since we are logging
+    pd.set_option('display.max_colwidth', 300)
+
     logger = logging.getLogger('baus')  # Set up a named logger for the main BAUS module
 
     # Logging configuration
@@ -24,20 +30,27 @@ def setup_logging(log_file, log_level=logging.INFO):
         filename=log_file,
         filemode='w',     # Don't append - create new log
         level=log_level,  # Default level passed to the logger
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        # format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', # temp for debugging
+        format='%(name)s - %(levelname)s - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
     # Redirect stdout and stderr to the logger, capturing print statements for now
-    sys.stdout = StreamToLogger(logging.getLogger('baus'), logging.INFO)
+    sys.stdout = StreamToLogger(logging.getLogger('baus'), logging.DEBUG)  # current print is typically debug-level
     sys.stderr = StreamToLogger(logging.getLogger('baus'), logging.ERROR)
 
     logger.debug("baus logger:{}".format(logger))
-    # set orca logger to warn; info and debug aren't useful
-    logging.getLogger('orca').setLevel(logging.WARN)
-    logger.debug("orca logger:{}".format(logging.getLogger('orca')))
     # set these to info
-    logging.getLogger('urbansim.models.util').setLevel(logging.INFO)
-    logger.debug("urbansim.models.util logger:{}".format(logging.getLogger('urbansim.models.util')))
+    for logger_name in [
+        'orca',
+        'urbansim.models.util', 
+        'urbansim.models.supplydemand',
+        'urbansim.models.dcm',
+        'urbansim.models.regression',
+        'urbansim.models.transition',
+        'urbansim.urbanchoice.interaction']:
+
+        logging.getLogger(logger_name).setLevel(logging.INFO)
+        logger.debug("logger:{}".format(logging.getLogger(logger_name)))
 
     return logger
