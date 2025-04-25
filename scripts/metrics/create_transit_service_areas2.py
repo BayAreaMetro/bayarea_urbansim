75855f6f7dff6a50ad4d250f28c6b305fba0a70a
Joe Ahrenholtz <jahrenholtz@bayareametro.gov>
2025-04-25 11:48:44 -0700

    Update current stop universe for 2023

--- scripts/metrics/create_transit_service_areas2.py
+++ scripts/metrics/create_transit_service_areas2.py
@@ -12,7 +12,6 @@ ANALYSIS_CRS = "EPSG:26910"
 user = os.getlogin()
 home_dir = Path.home()
 box_dir = Path("E:/Box") if user.lower() in ['lzorn', 'jahrenholtz'] else home_dir / 'Box'
-m_drive = "M:/" if os.name == "nt" else "/Volumes/Data/Models"
 
 output_path = Path(
     box_dir,
@@ -26,19 +25,20 @@ output_path = Path(
 )
 
 high_quality_transit_stops_input_path = Path(
-    m_drive,
-    "Data",
-    "GIS layers",
-    "JobsHousingTransitProximity",
+    box_dir,
+    "Modeling and Surveys",
+    "Urban Modeling",
+    "Spatial",
+    "transit",
+    "transit_service_levels",
     "update_2025",
-    "inputs",
-    "proximity"
+    "inputs"
 )
 
-
+# Input files
 fbp_stops_file = os.path.join(high_quality_transit_stops_input_path, "FBP_HighQualityTransit_Metric.shp")
 dbp_stops_file = os.path.join(high_quality_transit_stops_input_path, "DBP_HighQualityTransit_Metric.shp")
-
+current_stops_file = os.path.join(high_quality_transit_stops_input_path, "2023_HighQualityTransit_Metric.shp")
 
 parcel_geo_file = Path(
     box_dir,
@@ -54,11 +54,11 @@ parcel_geo_file = Path(
 
 # Function to tag parcels within high quality transit stop buffers
 def tag_parcels_within_transit_stop_buffer(parcels_geo, stops_file, buffer_distance, logger, column_name):
-    logger.info(f"Loading high quality transit stops from {stops_file}")
+    logger.info(f"Loading transit stops from {stops_file}")
     stops = gpd.read_file(stops_file).to_crs(ANALYSIS_CRS)
     stop_buffer = station_buffer(stops, buf_dist=buffer_distance, logger=logger)
     
-    logger.info("Tagging parcels within high quality transit buffer")
+    # Spatial join of parcels and buffer
     parcels_within_buffer = gpd.sjoin(parcels_geo, stop_buffer, predicate="within", how="left")
     parcels_within_buffer[column_name] = parcels_within_buffer.index_right.notnull().astype(int)
     return parcels_within_buffer[["PARCEL_ID", column_name]]
@@ -71,23 +71,25 @@ def main():
     logger.info("Loading parcels data")
     parcels_geo = gpd.read_feather(parcel_geo_file).to_crs(ANALYSIS_CRS)
 
-    # Process DBP and FBP stops
+    # Process current stops for 2023 and 2050 stops for DBP and FBP
+    cur_parcels = tag_parcels_within_transit_stop_buffer(parcels_geo, current_stops_file, 0.5, logger, "cur")
     dbp_parcels = tag_parcels_within_transit_stop_buffer(parcels_geo, dbp_stops_file, 0.5, logger, "dbp")
     fbp_parcels = tag_parcels_within_transit_stop_buffer(parcels_geo, fbp_stops_file, 0.5, logger, "fbp")
 
     # Merge results
-    logger.info("Merging tagged DBP and FBP tagged parcels into a single dataframe")
+    logger.info("Merging parcels tagged by stop universe into a single dataframe")
     parcels_x_all_stop_buffer = dbp_parcels.merge(fbp_parcels, on="PARCEL_ID", how="outer")
+    parcels_x_all_stop_buffer = parcels_x_all_stop_buffer.merge(cur_parcels, on="PARCEL_ID", how="outer")
 
-    # Add NP column (identical to DBP)
+    # NP and DBP have the same transit network - Add an NP column
     parcels_x_all_stop_buffer["np"] = parcels_x_all_stop_buffer["dbp"]
 
-    # Fill missing values with 0 and rename columns
-    parcels_x_all_stop_buffer = parcels_x_all_stop_buffer.fillna(0).astype({"dbp": int, "fbp": int, "np": int})
+    # Fill missing values with 0 and rename id column
+    parcels_x_all_stop_buffer = parcels_x_all_stop_buffer.fillna(0).astype({"dbp": int, "fbp": int, "np": int, "cur": int})
     parcels_x_all_stop_buffer.rename(columns={"PARCEL_ID": "parcel_id"}, inplace=True)
 
     # Reorder columns
-    parcels_x_all_stop_buffer = parcels_x_all_stop_buffer[["parcel_id", "np", "dbp", "fbp"]]
+    parcels_x_all_stop_buffer = parcels_x_all_stop_buffer[["parcel_id", "cur", "np", "dbp", "fbp"]]
 
     # Save to CSV
     output_file = output_path / "parcels10_x_high_quality_stop_buffer.csv"
