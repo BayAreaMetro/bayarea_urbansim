2c79cbd794c7a5a24642d45d4072bfd66cb3aead
Joe Ahrenholtz <jahrenholtz@bayareametro.gov>
2025-05-13 19:49:36 -0700

    Add EIR alternative to scenario mapping

--- scripts/metrics/metrics_connected.py
+++ scripts/metrics/metrics_connected.py
@@ -448,8 +448,9 @@ def transit_service_area_share_v2(
         "DBP": "dbp", 
         "Alt1": "fbp",
         "Alt2": "fbp",
-        "EIR Alt 1":'fbp',
-        "EIR Alt 2":'fbp',
+        'EIR': 'fbp',
+        'EIR Alt 2': 'fbp',
+        'EIR Alt 2 Var 1': 'fbp',
         "Current": "cur", # refers to existing conditions transit stops
     }
     
@@ -517,7 +518,7 @@ def transit_service_area_share_v2(
         if is_baseyear:
             if modelrun_alias == 'No Project':
                 logging.info(f'Processing baseyear {year} for No Project.')
-            elif modelrun_alias in ['Draft Blueprint', 'Final Blueprint']:
+            elif modelrun_alias in ['Draft Blueprint', 'Final Blueprint', 'EIR Alt 2', 'EIR Alt 2 Var 1']:
                 logging.info(f'Skipping baseyear {year} for {modelrun_alias}.')
                 continue
             else:
@@ -526,7 +527,8 @@ def transit_service_area_share_v2(
 
         # Process 2050 for No Project, Draft Blueprint, and Final Blueprint
         elif year == 2050:
-            if modelrun_alias in ['No Project', 'Draft Blueprint', 'Final Blueprint']:
+            if modelrun_alias in ['No Project', 'Draft Blueprint', 'Final Blueprint', 
+                                  'EIR Alt 2', 'EIR Alt 2 Var 1']:
                 logging.info(f'Processing {year} for {modelrun_alias}.')
             else:
                 logging.info(f'Skipping {year} for {modelrun_alias}.')
