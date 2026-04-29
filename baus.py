ade729ca0b962038ebda0b086eeb00afec27e87e
Aksel Olsen <aolsen@bayareametro.gov>
2026-04-29 12:19:43 -0700

    Use main version of baus.py
    Replaced merged baus.py with main branch version to get all B1 enhancements
    (modular slack, configurable years, enhanced CLI) cleanly without conflicts.

--- baus.py
+++ baus.py
@@ -29,72 +29,28 @@ import orca
 import orca_test
 import pandana
 import shutil
-import yaml
 import logging
 
-<<<<<<< HEAD
-from logging_setup import setup_logging, get_log_level
-
-MODE = "simulation"
-EVERY_NTH_YEAR = 5
-IN_YEAR, OUT_YEAR = 2010, 2050
-years_to_run = range(IN_YEAR+EVERY_NTH_YEAR, OUT_YEAR+1, EVERY_NTH_YEAR)
-=======
->>>>>>> main
         
+# Get repo deets        
 CURRENT_BRANCH = os.popen('git rev-parse --abbrev-ref HEAD').read().rstrip()
 CURRENT_COMMIT = os.popen('git rev-parse HEAD').read().rstrip()
 
-SLACK = "URBANSIM_SLACK" in os.environ
-if SLACK:
-    host = socket.gethostname()
-    from slack_sdk import WebClient
-    from slack_sdk.errors import SlackApiError
-    client = WebClient(token=os.environ["SLACK_TOKEN"])
-    slack_channel = "#urbansim_sim_update"
-
-
+# Configure argument parsing
 parser = argparse.ArgumentParser(description='Run UrbanSim models.')
 
-<<<<<<< HEAD
-parser.add_argument('-p','--yaml_path', default='run_setup.yaml', dest='yaml', help='Path to the YAML file with simulation setup details')
-parser.add_argument('--mode', action='store', dest='mode', help='which mode to run (see code for mode options)')
-parser.add_argument('-i', action='store_true', dest='interactive', help='enter interactive mode after imports')
-parser.add_argument('--set-random-seed', action='store_true', dest='set_random_seed', default=True, help='set a random seed for consistent stochastic output')
-=======
 parser.add_argument('--run_setup_yaml', default='run_setup.yaml', help='Specify run_setup.yaml file to use')
 parser.add_argument('--mode', action='store', dest='mode', default='simulation', help='which mode to run (see code for mode options)')
 parser.add_argument('-i', action='store_true', dest='interactive', default=False, help='enter interactive mode after imports')
 parser.add_argument('--set-random-seed', action='store_true', dest='set_random_seed', default=False, help='set a random seed for consistent stochastic output')
->>>>>>> main
 parser.add_argument('--disable-slack', action='store_true', dest='no_slack', default=False, help='disable slack outputs')
 parser.add_argument('--enable-asana', action='store_true', dest='use_asana', default=False, help='disable Asana task creation')
-parser.add_argument(
-    '--logging-detail',
-    action='store',
-    dest='logging_detail',
-    default='medium',
-    choices=['low', 'medium', 'high'],
-    help='Set format detail to low / medium / high'
-)
-parser.add_argument(
-    '--logging-level',
-    action='store',
-    dest='logging_level',
-    default='INFO',  # Default log level
-    choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-    help='Set log level detail'
-)
 
 options = parser.parse_args()
 
-if options.interactive:
-    INTERACT = True
+# Harvest constants - 
+INTERACT = options.interactive
 
-<<<<<<< HEAD
-if options.mode:
-    MODE = options.mode
-=======
 # use the given run_setup.yaml file
 orca.add_injectable("run_setup_yaml", options.run_setup_yaml)
 
@@ -135,16 +91,12 @@ if SLACK:
     slack_channel = "#urbansim_sim_update"
     orca.add_injectable('slack_client',client)
     orca.add_injectable('slack_channel',slack_channel)
->>>>>>> main
 
 if options.set_random_seed:
     SET_RANDOM_SEED = True
     np.random.seed(42)
 else:
     SET_RANDOM_SEED = False
-
-if options.no_slack:
-    SLACK = False
 
 if options.use_asana:
     ASANA = True
@@ -153,20 +105,10 @@ if options.use_asana:
     create_asana_task_from_yaml,
     add_comment_to_task, 
     mark_task_as_complete)
+    # hard code this for now
     ASANA_SECTION_NAME = 'Final Blueprint Runs'
 else:
     ASANA = False
-
-orca.add_injectable("years_per_iter", EVERY_NTH_YEAR)
-orca.add_injectable("base_year", IN_YEAR)
-orca.add_injectable("final_year", OUT_YEAR)
-orca.add_injectable("run_setup_path", options.yaml)
-
-
-run_setup = orca.get_injectable("run_setup")
-run_name = orca.get_injectable("run_name")
-outputs_dir = pathlib.Path(orca.get_injectable("outputs_dir"))
-outputs_dir.mkdir(parents=True, exist_ok=True)
 
 
 # Set up BAUS logging to write to the specified log file
@@ -181,21 +123,11 @@ except Exception as inst:
     print(inst)
     sys.exit()
 
-<<<<<<< HEAD
-log_level = get_log_level(options.logging_level)
-
-logger = setup_logging(log_file_path, log_level, detail_level=options.logging_detail)
-
-=======
 orca.add_injectable('logger',logger)
->>>>>>> main
 logger.info("***The standard stream is being written to the log file***")
-
 logger.info("Started: %s", time.ctime())
 logger.info("Current Branch: %s", CURRENT_BRANCH)
 logger.info("Current Commit: %s", CURRENT_COMMIT)
-logger.info("Local Clone Location: %s", os.path.abspath(sys.argv[0]))
-logger.info("Current Host: %s", host)
 logger.info("Set Random Seed: %s", SET_RANDOM_SEED)
 logger.info("Python version: %s", sys.version.split('|')[0])
 logger.info("UrbanSim version: %s", urbansim.__version__)
@@ -205,15 +137,9 @@ logger.info("Orca Test version: %s", orca_test.__version__)
 logger.info("Pandana version: %s", pandana.__version__)
 logger.info("Numpy version: %s", np.__version__)
 logger.info("Pandas version: %s", pd.__version__)
-logger.info("run_setup.yaml config: %s", options.yaml)
 
 logger.info("SLACK: %s", SLACK)
 logger.info("MODE: %s", MODE)
-
-# Memorialize the run config with the outputs - goes by run name attribute
-
-logger.info(f'***Copying {options.yaml} to output directory')
-shutil.copyfile(options.yaml, os.path.join(orca.get_injectable("outputs_dir"), f'run_setup_{run_name}.yaml'))
 
 
 def run_models(mode):
@@ -310,120 +236,6 @@ def run_models(mode):
             "region_marginals",
         ]
             
-<<<<<<< HEAD
-            baseyear_metrics_models = [
-
-                "growth_geography_metrics",
-                "deed_restricted_units_metrics",
-                "household_income_metrics",
-                "equity_metrics",
-                "jobs_housing_metrics",
-                "jobs_metrics",
-                "slr_metrics",
-                "earthquake_metrics",
-                "greenfield_metrics",
-            ]
-
-            return baseyear_metrics_models
-    
-        def get_simulation_models():
-        
-            simulation_models = [
-
-                "slr_inundate",
-                "slr_remove_dev",
-                "eq_code_buildings",
-                "earthquake_demolish",
-
-                "neighborhood_vars",   
-                "regional_vars",      
-
-                "nrh_simulate",
-
-                "household_relocation",
-                "households_transition",
-
-                "reconcile_unplaced_households",
-
-                "jobs_relocation",
-                "jobs_transition",
-
-                "balance_rental_and_ownership_hedonics",
-
-                "price_vars",
-                "scheduled_development_events",
-
-                "preserve_affordable",
-
-                "lump_sum_accounts",
-                "subsidized_residential_developer_lump_sum_accts",
-
-
-                "office_lump_sum_accounts",
-                "subsidized_office_developer_lump_sum_accts",
-
-                "alt_feasibility", # instead - we do orca.eval_step("alt_feasibility") inside each developer model
-    
-                "subsidized_residential_feasibility",
-                "subsidized_residential_developer_vmt",
-        #        "subsidized_residential_feasibility",
-        #        "subsidized_residential_developer_jobs_housing",
-
-                #"alt_feasibility", this could override the feasib calcs from "subsidized_residential_feasibility",
-                "residential_developer",
-                "developer_reprocess",
-
-                #"alt_feasibility", this could override the feasib calcs from "subsidized_residential_feasibility",
-                
-                "retail_developer",
-
-                #"alt_feasibility", this could override the feasib calcs from "subsidized_residential_feasibility",
-                
-                "office_developer",
-                "subsidized_office_developer_vmt",
-
-                "accessory_units_strategy",
-                "calculate_vmt_fees",
-
-                "remove_old_units",
-                "initialize_new_units",
-                "reconcile_unplaced_households",
-
-                "rsh_simulate",   
-                "rrh_simulate", 
-
-                "assign_tenure_to_new_units",
-
-                "hlcm_owner_lowincome_simulate",
-                "hlcm_renter_lowincome_simulate",
-
-                # the hlcms above could be moved above the developer again, 
-                # but we would have to run the hedonics and assign tenure to units twice
-                "hlcm_owner_simulate",
-                "hlcm_renter_simulate",
-                "hlcm_owner_simulate_no_unplaced",
-                "hlcm_owner_lowincome_simulate_no_unplaced",
-                "hlcm_renter_simulate_no_unplaced",
-                "hlcm_renter_lowincome_simulate_no_unplaced",
-
-                "reconcile_placed_households",
-
-                "proportional_elcm",
-                "gov_transit_elcm",
-                "elcm_simulate_ec5",
-                "elcm_simulate",  
-
-                "calculate_vmt_fees",
-                "calculate_jobs_housing_fees"]
-
-            if not run_setup["run_jobs_to_transit_strategy_elcm"]:
-                simulation_models.remove("elcm_simulate_ec5")
-                logger.info('Removing `elcm_simulate_ec5`')
-
-            if not run_setup["run_jobs_to_transit_strategy_random"]:
-                simulation_models.remove("gov_transit_elcm")
-                logger.info('Removing `gov_transit_elcm`')
-=======
         baseyear_metrics_models = [
             "growth_geography_metrics",
             "deed_restricted_units_metrics",
@@ -435,7 +247,6 @@ def run_models(mode):
             "earthquake_metrics",
             "greenfield_metrics",
         ]
->>>>>>> main
             
         simulation_models = [
             "debug",
@@ -602,65 +413,6 @@ def run_models(mode):
             "add_to_model_run_inventory_file"
         ]
 
-<<<<<<< HEAD
-                "parcel_summary",
-                "parcel_growth_summary",
-                "building_summary",
-
-                "hazards_slr_summary",
-                "hazards_eq_summary",
-
-                "deed_restricted_units_summary",
-                "deed_restricted_units_growth_summary",
-
-                "geographic_summary",
-                "geographic_growth_summary",
-                "parcel_transitions",
-                "taz1_summary",
-                "maz_marginals",
-                "maz_summary",
-                "taz2_marginals",
-                "county_marginals",
-                "region_marginals",
-                "taz1_growth_summary",
-                "maz_growth_summary",
-
-            ]
-
-            if not run_setup["run_slr_summaries"]:
-                simulation_summary_models.remove("hazards_slr_summary")
-            return simulation_summary_models
-        
-
-        def get_simulation_metrics_models():
-            
-            simulation_metrics_models = [
-
-                "growth_geography_metrics",
-                "deed_restricted_units_metrics",
-                "household_income_metrics",
-                "equity_metrics",
-                "jobs_housing_metrics",
-                "jobs_metrics",
-                "slr_metrics",
-                "earthquake_metrics",
-                "greenfield_metrics",
-            ]
-
-            return simulation_metrics_models
-        
-        def get_simulation_visualization_models():
-
-            simulation_visualization_models = [
-                "copy_files_to_viz_loc",
-                "add_to_model_run_inventory_file"
-            ]
-
-            return simulation_visualization_models
-
-        baseyear_models = get_baseyear_models()
-=======
->>>>>>> main
         if run_setup["run_summaries"]:
             baseyear_models.extend(baseyear_summary_models)
         if run_setup["run_metrics"]:
@@ -690,12 +442,7 @@ def run_models(mode):
         orca.run(simulation_models, iter_vars=years_to_run)
 
         if run_setup["run_visualizer"]:
-<<<<<<< HEAD
-            visualization_models = get_simulation_visualization_models()
-            orca.run(visualization_models, iter_vars=[years_to_run[-1]])
-=======
             orca.run(simulation_visualization_models, iter_vars=[STOP_YEAR])
->>>>>>> main
             
 
     elif mode == "visualizer":
@@ -709,56 +456,17 @@ def run_models(mode):
         raise "Invalid mode"
 
 
-
-
 if ASANA:
-<<<<<<< HEAD
-    # Get run_setup.yaml from options.yaml
-    task_handle = create_asana_task_from_yaml(options.yaml, run_name, ASANA_SECTION_NAME)
-=======
     # We can do this before the shutil copy step and just use the native run_setup.yaml in the same dir as baus.py
     task_handle = create_asana_task_from_yaml(options.run_setup_yaml, run_name, ASANA_SECTION_NAME)
->>>>>>> main
 
     # Get task identifer for later comment posting 
     task_gid = task_handle['gid']
 
     logger.info(f"Creating asana run task with URL: {task_handle['permalink_url']}")
 
+# Memorialize the run config with the outputs - goes by run name attribute
 
-<<<<<<< HEAD
-if SLACK and MODE == "estimation":
-    slack_start_message = f'Starting estimation {run_name} on host {host}'
-    try:
-        # For first slack channel posting of a run, catch any auth errors
-        init_response = client.chat_postMessage(channel=slack_channel,
-                                                text=slack_start_message)
-    except SlackApiError as e:
-        assert e.response["ok"] is False
-        assert e.response["error"]  
-        logger.info(f"Slack Channel Connection Error: {e.response['error']}")
-
-if SLACK and MODE == "simulation":
-    slack_start_message = f'Starting simulation {run_name} on host {host}\nOutput written to: {run_setup["outputs_dir"]}'
-    
-    try:
-        # For first slack channel posting of a run, catch any auth errors
-        init_response = client.chat_postMessage(channel=slack_channel,
-                                           text=slack_start_message)
-
-        if ASANA:
-
-            asana_msg = f"Creating asana run task with URL: {task_handle['permalink_url']}"
-            asana_response = client.chat_postMessage(channel=slack_channel,
-                                    thread_ts=init_response.data['ts'],
-                                    text=asana_msg)
-
-    except SlackApiError as e:
-        assert e.response["ok"] is False
-        assert e.response["error"]  
-        logger.info(f"Slack Channel Connection Error: {e.response['error']}")
-
-=======
 logger.info(f'***Copying {options.run_setup_yaml} to output directory')
 shutil.copyfile(options.run_setup_yaml, os.path.join(orca.get_injectable("outputs_dir"), f'run_setup_{run_name}.yaml'))
 
@@ -771,42 +479,9 @@ if ASANA:
         text=asana_msg)
 
 # main event: run the models
->>>>>>> main
 try:
     run_models(MODE)
 
-<<<<<<< HEAD
-    # Run alt travel model summary functions for 2030 and 2040
-    # This is a temporary hack.  See Asana task: https://app.asana.com/1/11860278793487/project/1209436408768030/task/1210468750496595
-    if MODE == "simulation":
-        alt_years = [2030, 2040]
-        for year in alt_years:
-            # Get required orca tables
-            maz = orca.get_table('maz')
-            tm1_tm2_maz_forecast_inputs = orca.get_table('tm1_tm2_maz_forecast_inputs')
-            tm1_tm2_regional_demographic_forecast = orca.get_table('tm1_tm2_regional_demographic_forecast')
-            tm2_emp27_employment_shares = orca.get_table('tm2_emp27_employment_shares')
-            tm1_tm2_regional_controls = orca.get_table('tm1_tm2_regional_controls')
-            tm2_taz2_forecast_inputs = orca.get_table('tm2_taz2_forecast_inputs')
-            tm2_occupation_shares = orca.get_table('tm2_occupation_shares')
-            run_name = orca.get_injectable('run_name')
-
-            # Call the alt TM functions to fun in isolation from other summaries
-            travel_model_summaries.maz_marginals_alt(
-                maz, year, tm1_tm2_maz_forecast_inputs, tm1_tm2_regional_demographic_forecast, run_name
-            )
-            travel_model_summaries.maz_summary_alt(
-                maz, year, tm2_emp27_employment_shares, tm1_tm2_regional_controls, run_name
-            )
-            travel_model_summaries.taz2_marginals_alt(
-                tm2_taz2_forecast_inputs, tm1_tm2_regional_demographic_forecast, tm1_tm2_regional_controls, year, run_name
-            )
-            travel_model_summaries.county_marginals_alt(
-                tm2_occupation_shares, year, run_name
-            )
-    
-=======
->>>>>>> main
 except Exception as e:
     logger.info(traceback.print_exc())
     tb = e.__traceback__
@@ -836,32 +511,6 @@ except Exception as e:
         raise e
     sys.exit(0)
 
-<<<<<<< HEAD
-if SLACK and MODE == "simulation":
-    slack_completion_message = f'Completed simulation {run_name} on host {host}'
-    response = client.chat_postMessage(channel=slack_channel,
-                                       thread_ts=init_response.data['ts'],
-                                       text=slack_completion_message)
-
-    
-    if ASANA:
-        # Add a comment
-        add_comment_to_task(task_gid, "Simulation completed successfully.")
-
-        # Mark the task as completed
-        mark_task_as_complete(task_gid)
-
-        response = client.chat_postMessage(channel=slack_channel,
-                                       thread_ts=init_response.data['ts'],
-                                       text='Check asana for details.')
-
-
-    
-
-
-                                                                                            
-logger.info("Finished: %s", time.ctime())
-=======
 if SLACK: baus.slack.slack_complete(MODE, host, run_name)
 
 if ASANA:
@@ -876,4 +525,3 @@ if ASANA:
                                        text='Check asana for details.')
 
 print("Finished", time.ctime())
->>>>>>> main
